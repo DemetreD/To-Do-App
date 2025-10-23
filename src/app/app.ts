704a8fa5aadd70a6f4be18c77a072d8d66fd2a52
Demetre <136166143+DemetreD@users.noreply.github.com>
2025-10-23 11:27:42 -0700

    Update ts file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './sidebar/sidebar';
+import { Tasklist } from './tasklist/tasklist';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Sidebar,Tasklist],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('to-do');
+  protected readonly title = signal('todo-app');
+
+  activeCategory: string = 'All'; 
+
+
+  onCategoryChange(selected: string) {
+    this.activeCategory = selected;
+  }
+
 }
